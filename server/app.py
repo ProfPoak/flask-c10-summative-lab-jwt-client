8a8aaae39b2b958eeefafa844b7578413d3f3d32
Porter <pdhakes25@gmail.com>
2026-07-22 15:11:00 -0600

    created CheckSession route

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import request, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
-from flask_jwt_extended import create_access_token, verify_jwt_in_request, get_jwt_identity
+from flask_jwt_extended import create_access_token, verify_jwt_in_request, get_jwt_identity, jwt_required
 
 from config import app, db, api, jwt
 from models import User, UserSchema
@@ -31,7 +31,15 @@ class Signup(Resource):
             201
         )
 
+class CheckSession(Resource):
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+        user = User.query.get(user_id)
+        return UserSchema().dump(user), 200
+        
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
