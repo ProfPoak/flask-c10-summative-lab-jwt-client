f6d6ca6bf785e5f38a0725827c7cfb8030f27f77
Porter <pdhakes25@gmail.com>
2026-07-22 14:56:48 -0600

    working signup route

--- server/app.py
+++ server/app.py
@@ -1,11 +1,37 @@
-from flask import request
+from flask import request, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token, verify_jwt_in_request, get_jwt_identity
 
 from config import app, db, api, jwt
 from models import User, UserSchema
 
+class Signup(Resource):
+    def post(self):
+        try:
+            json = request.get_json()
+            user = User(
+                username=json['username']
+            )
 
+            user.password_hash = json['password']
+
+            db.session.add(user)
+            db.session.commit()
+        except (KeyError, ValueError, IntegrityError) as e:
+            db.session.rollback()
+            return {'errors': [str(e)]}, 422
+
+        access_token = create_access_token(identity=str(user.id))
+        return make_response(
+            jsonify(
+                token=access_token, 
+                user=UserSchema().dump(user)
+            ),
+            201
+        )
+
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
