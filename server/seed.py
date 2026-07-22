4e305fd19215cf19d7a67926b4b6406fbf8f35ef
Porter <pdhakes25@gmail.com>
2026-07-22 11:25:28 -0600

    setup seed.py scaffold

---
+++ server/seed.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from random import randint, choice as rc
+
+from faker import Faker
+
+from app import app
+from models import db, User
+
+fake = Faker()
+
+with app.app_context():
+    pass
